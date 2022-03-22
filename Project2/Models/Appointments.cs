737c98a8fcd3a0425d02f644cae594cc2c82a219
Sam Robertson <robertsonnew@gmail.com>
2022-03-21 20:56:32 -0600

    Initial check-in of module Project2

---
+++ Project2/Models/Appointments.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project2.Models
+{
+    public class Appointments
+    {
+        [Key]
+        [Required]
+        public long ApptId { get; set; }
+        [Required]
+        public string groupName { get; set; }
+        [Required]
+        [MaxLength(15)] // I think this is right...
+        public int groupSize { get; set; }
+        [Required]
+        public string Email { get; set; }
+ 
+        public string Phone { get; set; }
+    }
+}
+
---
+++ Project2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Project2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
